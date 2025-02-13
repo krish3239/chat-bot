f4f34d75e7682f89f9ac605754ebbebea8136c28
krish3239 <krishbhardwaj3239@gmail.com>
2025-02-13 12:00:47 +0530

    minor bug of get fixed

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,7 +4,7 @@ import toast from "react-hot-toast";
 import {io} from 'socket.io-client'
 
 const BASE_URL='http://localhost:5001'
-export const useAuthStore = create((set) => ({
+export const useAuthStore = create((set,get) => ({
   authUser: null,
   isSigningUp: false,
   isLoggingIn: false,
