951eb472f749d7f2d4e36debdddfb5aeabc22c0d
krish3239 <krishbhardwaj3239@gmail.com>
2025-02-13 11:33:45 +0530

    port changed

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,7 @@ import cors from "cors"
 import {app,server } from "./lib/socket.js";
 
 dotenv.config();
-const PORT=process.env.PORT;
+const PORT=process.env.PORT||5001;
 app.use(express.json({ limit: "10mb" }));  // Increase JSON payload limit to 10MB
 app.use(express.urlencoded({ limit: "10mb", extended: true })); // For form data
 app.use(cookieParser());
